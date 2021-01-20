4a94424324ab0dbe448b6383b9e5aab8484c5e8d
bbbkx <33077574+bbbkx123@users.noreply.github.com>
2021-01-20 17:24:25 +0800

    notice开发

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Menu, Avatar, Dropdown, Space } from 'antd';
 import {
   LogoutOutlined,
   QuestionCircleOutlined,
   UserOutlined,
+  MessageOutlined,
 } from '@ant-design/icons';
 //@ts-ignore
 import { history, useModel, Link, SelectLang, useIntl } from 'umi';
 import styles from './index.less';
 import { Tooltip, Tag } from 'antd';
 import headimg from '@/assets/images/headimg.jpeg';
+import Notice from '@/components/Notice';
 
 interface LocalData {
   lang: string;
@@ -73,6 +75,60 @@ function RightContent(Props: any) {
     return locales;
   };
 
+  const { notification, setNotification } = useModel('useNoticeModel');
+  console.log(useModel('useNoticeModel'));
+
+  useEffect(() => {
+    setNotification([
+      {
+        avatar:
+          'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
+        title: '你收到了 14 份新周报',
+        datetime: '3 年前',
+        read: false,
+      },
+      {
+        avatar:
+          'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png',
+        title: '你推荐的 曲妮妮 已通过第三轮面试',
+        datetime: '3 年前',
+        read: false,
+      },
+      {
+        avatar:
+          'https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png',
+        title: '这种模板可以区分多种通知类型',
+        datetime: '3 年前',
+        read: false,
+      },
+    ]);
+  }, []);
+
+  const onClick1 = (item: any, index: number) => {
+    // debugger
+    // if (!notification[index].read) {
+
+    // }
+    // notification[index].read = true
+    notification[index].title = '小丑竟是我自己';
+    setNotification(notification);
+
+    setTimeout(() => console.log(notification), 3000);
+  };
+
+  const notice = (
+    <div className={styles.notice}>
+      <Notice>
+        <Notice.Tab
+          tabKey="notification"
+          title="通知"
+          data={notification}
+          onClick={onClick1}
+        />
+      </Notice>
+    </div>
+  );
+
   return (
     <div className={className}>
       {process.env.UMI_ENV === 'local' && (
@@ -90,6 +146,13 @@ function RightContent(Props: any) {
           </a>
         </Tooltip>
       )}
+
+      <Dropdown overlay={notice} trigger={['hover']}>
+        <Space>
+          <MessageOutlined />
+        </Space>
+      </Dropdown>
+
       <Dropdown overlay={menu}>
         <Space
           style={{
