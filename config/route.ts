5715f5083920e019611203c0427d1a96549a8575
LLPing <2551157794@qq.com>
2021-02-02 17:36:28 +0800

    tubiao

--- config/route.ts
+++ config/route.ts
@@ -59,12 +59,12 @@ const routes: IBestAFSRoute[] = [
             name: '数据1',
             component: '@/pages/panel/dashBoard/index',
           },
-          {
-            locale: 'menu.panel.spot',
-            path: '/panel/spot',
-            name: '变型转换',
-            component: '@/pages/panel/spot/index',
-          },
+          // {
+          //   locale: 'menu.panel.spot',
+          //   path: '/panel/spot',
+          //   name: '变型转换',
+          //   component: '@/pages/panel/spot/index',
+          // },
           {
             locale: 'menu.panel.chart',
             path: '/panel/chart',
